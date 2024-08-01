7dbcb34ce61fb163ee212691e969c389621d3cf4
Pedro Vinícius de Almeida Silva Xavier <120400955+Pedrovinicius123@users.noreply.github.com>
2024-08-01 12:21:00 +0000

    Atividade turtle

---
+++ baesse/atv3/atividade_turtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+# QUESTÃO 1
+
+# Instanciando o objeto
+cursor1 = turtle.Turtle()
+
+# Aplicando alterações
+cursor1.screensize(200, 200)
+cursor1.color("blue")
+cursor1.foward(100)
+
+#QUESTÃO 2
